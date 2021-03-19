6f68ed8f146e8f6b046d6480a5420a36d0ca2236
playseeds <rachel@playseeds.com>
2021-03-19 12:37:33 -0700

    changed local host variable

--- server.js
+++ server.js
@@ -117,7 +117,7 @@ app.post('/webhook', async (req, res) => {
   res.sendStatus(200);
 });
 
-app.listen(4242, () => console.log(`Node server listening on port ${4242}!`));
+app.listen(process.env.PORT || 4242, () => console.log(`Node server listening on port ${process.env.PORT || 4242}!`));
 
 
 function checkEnv() {
